e883a8cf81c318fe382b40643eb4fe441f2b974d
nomad <st46619@student.upce.cz>
2019-04-21 18:59:28 +0200

    meeting - controller, service, entity, DAO

--- voting-system-application/src/main/java/cz/upce/votingsystemapplication/controller/MeetingController.java
+++ voting-system-application/src/main/java/cz/upce/votingsystemapplication/controller/MeetingController.java
@@ -1,11 +1,16 @@
 package cz.upce.votingsystemapplication.controller;
 
+import cz.upce.votingsystemapplication.entity.Meeting;
 import cz.upce.votingsystemapplication.service.MeetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.sql.Date;
 
 @Controller
 public class MeetingController {
@@ -27,5 +32,10 @@ public class MeetingController {
     public String getMeetingById(@PathVariable Long id, Model model) {
         model.addAttribute("meeting", meetingService.findById(id));
         return "meeting-detail";
+    }
+
+    @GetMapping("/meetings/add/{date}")
+    public void addMeeting(@PathVariable("date") String date, ModelMap model) {
+        meetingService.add(Date.valueOf(date));
     }
 }
