f5265860c47e45588d83e0aa3dc2f9c72c9468f5
venaca <venaca99@gmail.com>
2019-05-02 00:37:53 +0200

    FE-app init

---
+++ voting-system-app/src/components/Menu.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <v-layout justify-center>
+            <v-flex lg3 md4 sm12 px-5>
+                <v-layout align-space-around justify-center column fill-height px-10>
+                    <v-btn large color="primary">Login</v-btn>
+                    <v-btn large color="primary">Hlasování</v-btn>
+                    <v-btn large color="primary">Statistiky</v-btn>
+                    <v-btn large color="primary">Předchozí zastupitelstva</v-btn>
+                    <v-btn large color="primary">Správa návrhů</v-btn>
+                </v-layout>
+            </v-flex>
+        </v-layout>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Menu',
+        data() {
+            return {}
+        }
+    }
+</script>
