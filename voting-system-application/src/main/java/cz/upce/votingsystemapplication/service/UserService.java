d2849ee9ab2920d5c7088b13fb2c615f26272874
Petr Navrátil <pt.navratil@gmail.com>
2019-05-02 08:17:00 +0200

    pridano pouziti UserDto do Controlleru a Service

--- voting-system-application/src/main/java/cz/upce/votingsystemapplication/service/UserService.java
+++ voting-system-application/src/main/java/cz/upce/votingsystemapplication/service/UserService.java
@@ -1,10 +1,12 @@
 package cz.upce.votingsystemapplication.service;
 
 import cz.upce.votingsystemapplication.dao.UserDao;
+import cz.upce.votingsystemapplication.dto.UserDto;
 import cz.upce.votingsystemapplication.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,17 +20,23 @@ public class UserService {
         userDao.save(user);
     }
 
-    public User getUser(Long id) {
+    public UserDto getUser(Long id) {
         Optional<User> foundUser = userDao.findById(id);
         if (foundUser.isPresent()) {
-            return foundUser.get();
+            UserDto createdUserDto = new UserDto(foundUser.get().getId(), foundUser.get().getFirstName(), foundUser.get().getLastName(), foundUser.get().getEmail());
+            return createdUserDto;
         } else {
             return null;
         }
     }
 
-    public List<User> getAllUsers() {
-        return userDao.findAll();
+    public List<UserDto> getAllUsers() {
+        List<User> foundUsers = userDao.findAll();
+        List<UserDto> foundUserDtos = new ArrayList<>(foundUsers.size());
+        for (User foundUser : foundUsers) {
+            foundUserDtos.add(new UserDto(foundUser.getId(), foundUser.getFirstName(), foundUser.getLastName(), foundUser.getEmail()));
+        }
+        return foundUserDtos;
     }
 
     public void deleteUser(Long id) {
