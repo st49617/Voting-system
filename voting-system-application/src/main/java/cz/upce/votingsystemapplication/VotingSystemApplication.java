58709e65d297c96c6a183003bf751c90bd4f9fb8
Petr Navrátil <pt.navratil@gmail.com>
2019-05-08 16:06:41 +0200

    rozpracovana sprava uzivatelu na backendu, funkcni pridavani a zobrazeni seznamu

--- voting-system-application/src/main/java/cz/upce/votingsystemapplication/VotingSystemApplication.java
+++ voting-system-application/src/main/java/cz/upce/votingsystemapplication/VotingSystemApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class VotingSystemApplication {
 
     
