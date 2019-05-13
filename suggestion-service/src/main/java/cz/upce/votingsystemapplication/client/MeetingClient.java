ce3f6cb01aa2dada07b09c3c183b427ce40eb3a5
Ondrej Burda <ondrej.burda@plus4u.net>
2019-05-13 09:31:01 +0200

    Integration test

--- suggestion-service/src/main/java/cz/upce/votingsystemapplication/client/MeetingClient.java
+++ suggestion-service/src/main/java/cz/upce/votingsystemapplication/client/MeetingClient.java
@@ -5,6 +5,7 @@ import cz.upce.votingsystemapplication.dto.MeetingForSuggestionDto;
 import cz.upce.votingsystemapplication.dto.SuggestionForMeetingDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,4 +16,7 @@ public interface MeetingClient {
 
     @RequestMapping(method = RequestMethod.GET, value = "get/{id}")
     MeetingDto get(@PathVariable("id") Long id);
+
+    @RequestMapping(method = RequestMethod.POST, value = "add")
+    String add(@RequestBody MeetingDto meeting);
 }
