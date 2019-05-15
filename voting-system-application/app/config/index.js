c58db9fd2dce54324a41a27c78f9dd04ee9774ca
venaca <venaca99@gmail.com>
2019-05-15 17:09:09 +0200

    fungujici buildovani aplikace

--- voting-system-application/app/config/index.js
+++ voting-system-application/app/config/index.js
@@ -38,12 +38,14 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+//    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../../build-app/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+//    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../../build-app'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '',
 
     /**
      * Source Maps
