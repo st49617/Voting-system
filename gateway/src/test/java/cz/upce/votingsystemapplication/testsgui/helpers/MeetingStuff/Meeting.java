b206db6a5aaa6077503d81c0fc70436dc3fb47e8
Burdis28 <MrSpronx@gmail.com>
2019-05-20 01:06:23 +0200

    code cleanup

--- gateway/src/test/java/cz/upce/votingsystemapplication/testsgui/helpers/MeetingStuff/Meeting.java
+++ gateway/src/test/java/cz/upce/votingsystemapplication/testsgui/helpers/MeetingStuff/Meeting.java
@@ -2,9 +2,6 @@ package cz.upce.votingsystemapplication.testsgui.helpers.MeetingStuff;
 
 import java.sql.Timestamp;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 
 @Entity
